b867b1ef7f2ef8749be79375e7392492645aaa2f
Maroš Kolárik <maroskolarik@mail.com>
2023-08-14 11:03:05 +0200

    Add backend dir

---
+++ backend/src/main/java/com/gfa/backend/GreenBayApp.java
@@ -0,0 +1,13 @@
+package com.gfa.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenBayApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenBayApp.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/gfa/backend/GreenBayAppTests.java
@@ -0,0 +1,13 @@
+package com.gfa.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenBayAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
